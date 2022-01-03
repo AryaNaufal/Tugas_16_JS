874f6dee528b6efee0fb30d7a7b61bbcf7015a90
Arya Naufal <arya.naufal.andt@gmail.com>
2022-01-03 22:17:03 +0700

    TUGAS 16 JS
    Tugas ini kami kumpulkan untuk Niomic

---
+++ tugas16.js
@@ -0,0 +1,6 @@
+var Minimal = Math.min(45, 100, 99, 3, 2, 4, 1, 2, 3, 19, 22, 14, 10);
+var Maximal = Math.max(45, 100, 99, 3, 2, 81, 22, 55, 1, 2, 3, 4, 5);
+
+
+console.log(Minimal)
+console.log(Maximal)
